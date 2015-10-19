8d324e0c5672c864cb0d1a8c34014fdfa871a981
Robin Radic <robin@radic.nl>
2015-10-19 18:59:18 +0200

    pre-tag build

--- resources/assets/jquery.validate.laravel.js
+++ resources/assets/jquery.validate.laravel.js
@@ -14,22 +14,6 @@
   }
 }(this, function ($) {
 
-(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module unless amdModuleId is set
-    define(["jquery"], function (a0) {
-      return (factory(a0));
-    });
-  } else if (typeof exports === 'object') {
-    // Node. Does not work with strict CommonJS, but
-    // only CommonJS-like environments that support module.exports,
-    // like Node.
-    module.exports = factory(require("jquery"));
-  } else {
-    factory(jQuery);
-  }
-}(this, function ($) {
-
 var laraval;
 (function (laraval) {
     laraval.DEBUG = true;
@@ -926,9 +910,6 @@ var laraval;
 })(laraval || (laraval = {}));
 $.extend(true, $.validator, laraval.validator);
 $.validator.laraval = laraval;
-
-
-}));
 
 
 }));
